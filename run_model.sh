dffabd5030d4f27bed522c0727c51f5b7ca9e388
Sasha376slysenko <slysenko376@gmail.com>
2026-07-31 12:03:46 +0300

    Update run_model.sh

--- run_model.sh
+++ run_model.sh
@@ -5,15 +5,25 @@ BLUE='\033[0;36m'
 ORANGE='\033[38;5;208m'
 NC='\033[0;0m'
 
-RAPL_FILE='/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj'
+OS_TYPE="$(UNAME -S)"
 
-if [ -f "$RAPL_FILE" ] && [ ! -r "$RAPL_FILE" ]; then
-  echo -e "${ORANGE}[INFO] Надаємо доступ до апаратних лічильників RAPL${NC}"
-  sudo chmod -R a+r /sys/class/powercap/intel-rapl
+if [ "$OS_TYPE" = "Linux" ]; then
+  RAPL_FILE='/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj'
+
+  if [ -f "$RAPL_FILE" ] && [ ! -r "$RAPL_FILE" ]; then
+    echo -e "${ORANGE}[INFO] Надаємо доступ до апаратних лічильників RAPL${NC}"
+    sudo chmod -R a+r /sys/class/powercap/intel-rapl
+  fi
+  RUN_CMD="PYTHONPATH=. python modeling/main.py"
+elif [ "$OS_TYPE" = "Darwin" ]; then
+  echo -e "${ORANGE}[INFO macOS] Запуск із sudo для доступу до апаратних лічильників ${NC}"
+  RUN_CMD="sudo -E env \"PATH=$PATH\" \"PYTHONPATH=.\" python modeling/main.py"
+else
+  RUN_CMD="PYTHONPATH=. python modeling/main.py"
 fi
 
 echo -e "${GREEN} [START] Model${NC}"
 
-PYTHONPATH=. python modeling/main.py
+eval $RUN_CMD
 
 echo -e "${BLUE} [END] Model${NC}"
